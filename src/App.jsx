956bb4458dd6ccb0d64b863d8a54e9761703d4c8
Alona Harnyk <alyonagarnyk@gmail.com>
2025-05-18 11:04:36 +0300

    add task 1 and task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import Heading from './components/Heading/Heading';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import {lazy, Suspense} from 'react';
+import Header from './components/Header/Header'
+// import Home from './pages/Home'
+const Home = lazy(() => import('./pages/Home'))
+import SearchCountry from './pages/SearchCountry'
+import Country from './pages/Country'
 
 export const App = () => {
-  return <Heading title="App" bottom />;
+  return <>
+  <Header/>
+  <Suspense fallback={<div>Loading...</div>}>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/country' element={<SearchCountry/>}/>
+    <Route path='/country/:countryId' element={<Country/>}/>
+    <Route path='*' element={<Navigate to='/' replace/>}/>
+  </Routes>
+  </Suspense>
+  </>;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,40 @@
 import Container from '../components/Container/Container';
-import Heading from '../components/Heading/Heading';
 import Section from '../components/Section/Section';
+import CountryList from '../components/CountryList/CountryList';
+import Grid from '../components/Grid/Grid';
+import GridItem from '../components/GridItem/GridItem';
+import { useEffect, useState } from 'react';
+import { getCountries } from '../service/countryApi';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    // I
+    // const getData = async () => {
+    //   const data = await getCountries()
+    //   setCountries(data)
+    // }
+    // getData()
+
+    // II
+    getCountries().then(data => setCountries(data));
+  }, []);
+
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        <CountryList />
+        <Grid>
+          {countries.map(item => (
+            <GridItem key={item.id}>
+              <Link>
+                <img src={item.flag} alt={item.country} />
+              </Link>
+            </GridItem>
+          ))}
+        </Grid>
       </Container>
     </Section>
   );
